37f11c2b35784b95896eda25291568486821c0fa
Elektropay <elektropay.commerce@gmail.com>
2025-01-23 18:32:24 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Everpay - Payment Orchestration Platform",
-  description: "The fastest way to get paid from your customers",
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
 };
 
 const geistSans = Geist({
@@ -41,13 +41,12 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>EverPay</Link>
+                    <Link href={"/"}>Everpay</Link>
                     <div className="flex items-center gap-2">
-                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
+                      
                     </div>
                   </div>
-                
+                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
@@ -63,7 +62,7 @@ export default function RootLayout({
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Paylyfe 
+                    Paylyfe
                   </a>
                 </p>
                 <ThemeSwitcher />
