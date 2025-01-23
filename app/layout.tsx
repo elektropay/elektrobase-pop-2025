da6157fdcb88596287fd5211e5d71355bab21168
Elektropay <elektropay.commerce@gmail.com>
2025-01-23 18:38:15 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -41,9 +42,9 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Everpay</Link>
+                    <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
-                      
+                      <DeployButton />
                     </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
@@ -57,12 +58,12 @@ export default function RootLayout({
                 <p>
                   Powered by{" "}
                   <a
-                    href="https://paylyfe.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Paylyfe
+                    Supabase
                   </a>
                 </p>
                 <ThemeSwitcher />
