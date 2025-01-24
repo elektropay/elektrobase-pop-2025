d3dd991df02021f399f827f36f08688e244e4549
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 16:08:43 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,15 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
+    
+        <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
