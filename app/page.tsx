bddbb499ef33e90090f483196c702dde9ecc9977
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 01:09:47 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Hero from "@/components/hero";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Home() {
   return (
     <>
       <Hero />
       <div className="flex flex-col gap-16 items-center">
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Integrations</h2>
-        {hasEnvVars}
+        
       </main>
       </div>
     </>
