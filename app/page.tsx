5c8286c86decab185ea2cff5be5db5a34481ea04
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 00:50:47 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ export default async function Home() {
   return (
     <>
       <Hero />
+      <div className="flex flex-col gap-16 items-center">
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Integrations</h2>
         {hasEnvVars}
       </main>
+      </div>
     </>
   );
 }
