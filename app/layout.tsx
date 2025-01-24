fc6a7b1fbd196621f8f80b304b54fb1b29f1073b
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 01:20:52 -0500

    Update layout.tsx
    Added logo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import EverpayLogo from "./logo.png";
 import { Geist } from "next/font/google";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
@@ -41,7 +42,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Everpay</Link>
+                    <Link href={"/"}><EverpayLogo /></Link>
                     <div className="flex items-center gap-2">
                       
                     </div>
