9d246c00e76b4dd74040c94e2f4ce25e903837fc
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 02:50:05 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-5 py-16">
                 <p>
                   Powered by{" "}
                   <a
