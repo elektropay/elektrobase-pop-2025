0e2e4d5436c63ee15b86dff38fbfb11598d378d3
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 14:39:32 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,17 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}><NextLogo /></Link>
+                    <Link href={"/"}>
+      <a className="flex items-center space-x-2">
+        <Image
+          src="/logo.png"
+          alt="EverPay Logo"
+          width={124} 
+          height={36}
+          priority
+        />
+        <span className="text-lg font-semibold"></span>
+          </Link>
                     <div className="flex items-center gap-2">
                       
                     </div>
