4057eeced356713c735382762a5472e9033df83b
Elektropay <elektropay.commerce@gmail.com>
2025-01-23 19:38:14 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export const metadata = {
   title: "Everpay",
   description: "The fastest way to build apps with Next.js and Supabase",
 };
+const geistSans = GeistProvider({
+  display: "swap",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
