62bb498b88710103575c6940ecaf5473eab9f475
Elektropay <elektropay.commerce@gmail.com>
2025-01-24 01:49:24 -0500

    Update everpay-logo.tsx

--- components/everpay-logo.tsx
+++ components/everpay-logo.tsx
@@ -7,7 +7,8 @@ export default function EverpayLogo() {
       viewBox="0 0 120 37"
       width="120"
       xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="xMidYMid meet">
+      preserveAspectRatio="xMidYMid meet"
+  >
 
 <g transform="translate(0.000000,80.000000) scale(0.100000,-0.100000)"
 fill="#000000" stroke="none">
