8e1782044b56d07f6f1a448300c7778d57bcdef4
Elektropay <elektropay.commerce@gmail.com>
2025-01-25 19:40:56 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>
             <Image
-          src="/logo.svg" // Path to the PNG file in the public folder
+          src="./logo.svg" // Path to the PNG file in the public folder
           alt="EVERPAY"
           width={130} // Set the desired width
           height={30} // Adjust height to maintain aspect ratio
