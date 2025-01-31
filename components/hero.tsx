dac0d258413d634a19997e06503c3b72ef7c5d05
Elektropay <elektropay.commerce@gmail.com>
2025-01-31 16:21:57 -0500

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,46 +11,25 @@ export default async function Header() {
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
         {" "} The fastest way to accept customers payments{" "}
         <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+          href="https://elektrobase-pop-2025.vercel.app/sign-in"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Elektropay
+          Go to Dashboard
         </a>{" "}
         and {" "}
         <a
-          href="https://nextjs.org/"
+          href="https://elektrobase-pop-2025.vercel.app/sign-up"
           target="_blank"
           className="font-bold hover:underline"
           rel="noreferrer"
         >
-          Paylyfe.js
+          Sign up now
         </a>
       </p>
 
-      <div className="flex gap-8 justify-center items-center">
-        <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
-      </div>
+      
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
   );
