a55a8f85f37a16559fe122180d213246ec809419
Elektropay <elektropay.commerce@gmail.com>
2025-01-31 00:02:04 -0500

    Update header-auth.tsx

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -16,26 +16,7 @@ export default async function AuthButton() {
       <>
         <div className="flex gap-4 items-center">
           
-          <div className="flex gap-2">
-            <Button
-              asChild
-              size="sm"
-              variant={"outline"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size="sm"
-              variant={"default"}
-              disabled
-              className="opacity-75 cursor-none pointer-events-none"
-            >
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-          </div>
+          
         </div>
       </>
     );
