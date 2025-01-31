8ccf558e69db163906b1fbbceb353ebe2e071222
Elektropay <elektropay.commerce@gmail.com>
2025-01-31 16:24:01 -0500

    Update page.tsx
    Removed integration section

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ export default async function Home() {
   return (
     <>
       <Hero />
-      <div className="flex flex-col gap-16 items-center">
-      <main className="flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Integrations</h2>
-        
-      </main>
-      </div>
     </>
   );
 }
